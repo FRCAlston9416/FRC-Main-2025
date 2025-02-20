2b1f84e9eeb06b85df3a2e79f8543ccc55eb3a9d
Louqustic <157534017+Louqustic@users.noreply.github.com>
2025-02-20 16:49:56 -0500

    updated launcher.java to 2025 config stuff

--- src/main/java/frc/robot/subsystems/Launcher.java
+++ src/main/java/frc/robot/subsystems/Launcher.java
@@ -2,8 +2,13 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
@@ -21,8 +26,21 @@ public class Launcher extends SubsystemBase{
     
 
     public Launcher() {
-        launchMotor.setInverted(false);
-        secondaryMotor.setInverted(true);
+        //launchMotor.setInverted(false);
+        //secondaryMotor.setInverted(true);
+        SparkMaxConfig launchMotorConfig = new SparkMaxConfig();
+        launchMotorConfig
+            .inverted(false)
+            .idleMode(IdleMode.kBrake);
+        
+        launchMotor.configure(launchMotorConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+        
+        SparkMaxConfig secondaryMotorConfig = new SparkMaxConfig();
+        secondaryMotorConfig
+            .inverted(true)
+            .idleMode(IdleMode.kBrake);
+        
+        secondaryMotor.configure(secondaryMotorConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
         
     }
     // leftTrigger: backwards
